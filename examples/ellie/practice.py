35468be271fbf5bb551f222ffe41fb1b7a7d3926
Scott Person <jsperson@gmail.com>
2021-06-14 13:11:58 -0500

    Ellie practice

---
+++ examples/ellie/practice.py
@@ -0,0 +1,27 @@
+Jack = 34
+Erik = 51
+if Erik > Jack:
+    print('Erik is older than Jack')
+
+
+Happy = 50
+Sad = 100
+if Happy > Sad:
+    print('Laugh')
+elif Sad > Happy:
+    print('Shake your booty!')
+
+Color = 'Blue'
+if Color is 'Red':
+    print('The Color is Red!')
+elif Color is 'White':
+    print('The color is White!')
+else:
+    print('The Color is Blue!')
+
+if 1+1 == 3:
+    print('one plus one is three!')
+elif 2+2 == 4:
+    print('Two plus Two is four!')
+else:
+    print("Ican't math right now...")
