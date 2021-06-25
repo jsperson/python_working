7c1bd93be0f191a50a5670c112d92438b76a5985
scott.person@kochind.com <scott.person@kochind.com>
2021-06-25 09:25:47 -0500

    I have no idea

--- examples/requests_package/request_proxies.py
+++ examples/requests_package/request_proxies.py
@@ -1,14 +1,17 @@
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
 import requests
 from requests.auth import HTTPProxyAuth
 import os
+import pandas as pd
+
 file_path = os.path.dirname(os.path.abspath(__file__))
 
-from requests.utils import DEFAULT_CA_BUNDLE_PATH
 print(DEFAULT_CA_BUNDLE_PATH)
 
 proxies = {
-     'https': 'http://gateway.zscalertwo.net:443'
+    'https': 'http://gateway.zscalertwo.net:443'
 }
+
 
 r = requests.get(url='https://xkcd.com', proxies=proxies)
 print(r.status_code)
